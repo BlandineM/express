8e4f2f720d787645224d87293a7fec8b7dcbad8a
Blandine <blandine.mallart@gmail.com>
2019-12-29 14:45:18 +0100

    fil rouge

---
+++ conf.js
@@ -0,0 +1,8 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "localhost", // adresse du serveur
+  user: "root", // le nom d'utilisateur
+  password: "azerty", // le mot de passe
+  database: "database" // le nom de la base de données
+});
+module.exports = connection;
